2c1c9beeaf960d880086a7497e43ce1428dcd7cb
Joao Vitor <joao.vitor.garcia12@gmail.com>
2024-06-01 09:40:58 -0300

    fix whitespaces and remove unnecessary imports

--- comandsFunctions.py
+++ comandsFunctions.py
@@ -1,35 +1,24 @@
-import discord
-from discord.ext import commands
-from dotenv import load_dotenv
-import comandsFunctions
-import asyncio
 import os
-from datetime import timedelta
-from moviepy.editor import VideoFileClip
 from pytube import YouTube
 
 def dowloadVideo(link):
     try: 
-        
         yt = YouTube(link)
-
         dest_dir = 'audios/'
-
         ys = yt.streams.get_highest_resolution()
-
         ys = yt.streams.filter(only_audio=True).first()
+
         # Começa o download
         arquivo = ys.download(output_path=dest_dir, filename=ys.default_filename)
+
         #troca o nome do arquivo para mp3
         base, ext = os.path.splitext(arquivo) 
         novo_arquivo = base + '.mp3'
         os.rename(arquivo, novo_arquivo)
         dest_path = os.path.join(dest_dir, os.path.basename(novo_arquivo))
-        
+
         print(f'\nDownload concluído! {dest_path}')
         return dest_path
-
-        
     except Exception as e:
         print(f'Ocorreu um erro: {e}')
         exit()
