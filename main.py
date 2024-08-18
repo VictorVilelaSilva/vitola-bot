288974e57f784d7d6f5c283fb8a6b6ab9e172d02
Victor de Souza Vilela da Silva <79282009+VictorVilelaSilva@users.noreply.github.com>
2024-08-18 01:34:39 -0300

    faz com que o o main codigo rode o bot que agora é uma classe

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import bot
+from bot import DiscordBot
 from dotenv import load_dotenv
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_dotenv()
 
-    print('Running bot...')
-    bot.run_discord_bot()
+    print("Running bot...")
+    DiscordBot().run_discord_bot()
