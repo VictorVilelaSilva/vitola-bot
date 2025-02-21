b05b81ab4e81a1201718f667b06d6016462e9976
VictorVilela <victorsouzavilelasilva@gmail.com>
2025-02-21 15:57:46 -0300

    fix: move load_dotenv() call to the top of main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from src.bot import DiscordBot
 from dotenv import load_dotenv
+load_dotenv()
+from src.bot import DiscordBot
 
 if __name__ == "__main__":
-    load_dotenv()
 
     print("Running bot...")
     DiscordBot().run_discord_bot()
