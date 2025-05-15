88c1e9c463201866baf483e39a4a12a4ddd51e71
VictorVilela <victorsouzavilelasilva@gmail.com>
2025-05-15 17:59:17 -0300

    Adicionando nova estrutura de pastas do bot

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from dotenv import load_dotenv
 load_dotenv()
 from src.bot import DiscordBot
+import asyncio
+
+async def setup():
+    bot_instance = DiscordBot()
+    bot = bot_instance.init_bot()
+    
+    # Adicionar referência do bot_instance ao bot
+    bot.bot_instance = bot_instance
+
+    # Carregue as extensões de forma assíncrona
+    await bot.load_extension("src.cogs.music")
+    #await bot.load_extension("src.cogs.chat")
+    # ...carregue mais Cogs aqui...
+
+    return bot, bot_instance
 
 if __name__ == "__main__":
-
+    # Configurar e iniciar o bot de forma assíncrona
+    loop = asyncio.get_event_loop()
+    bot, bot_instance = loop.run_until_complete(setup())
+    
     print("Running bot...")
-    DiscordBot().run_discord_bot()
+    bot.run(bot_instance.TOKEN)
