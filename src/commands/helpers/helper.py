1ea0c73408733670d5dec5564c75f9bb2fd4b9f6
Victor de Souza Vilela da Silva <victorsouzavilelasilva@gmail.com>
2025-05-15 22:42:45 -0300

    fix: corrige instância do YouTube para usar use_po_token=True na função download_video

--- src/commands/helpers/helper.py
+++ src/commands/helpers/helper.py
@@ -3,7 +3,7 @@ from pytubefix import YouTube
 
 def download_video(link):
     try:
-        yt = YouTube(link,client="WEB")
+        yt = YouTube(link,use_po_token=True)
         yt_title = yt.title
         dest_dir = os.path.join(os.getcwd(), "src/assets/tempAudios/")
         ys = yt.streams.get_highest_resolution()
