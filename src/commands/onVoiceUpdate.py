3642a336b627fce3eaf16971c8df6f0cb6896b2c
Victor de Souza Vilela da Silva <victorsouzavilelasilva@gmail.com>
2025-07-05 12:21:42 -0300

    fix: adiciona referência ao áudio 'Edvaldo.mp3' no mapeamento de áudio

--- src/commands/onVoiceUpdate.py
+++ src/commands/onVoiceUpdate.py
@@ -9,6 +9,7 @@ async def handle_on_voice_update(bot, member, after):
         "gustavotoaiari": "Gustavo.mp3",
         "brunodss": "Bruno.mp3",
         "dino.l": "Dino.mp3",
+        "modesto1":"Edvaldo.mp3"
         #"vitolaapenas": "Victor.mp3",
     }
     filename = audio_map.get(member.name)
