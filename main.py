96814e42424606cf1d48f1db683f2f4d63d4670d
Victor de Souza Vilela da Silva <victorsouzavilelasilva@gmail.com>
2025-08-13 19:52:55 -0300

    feat: refatora a função de configuração do bot para carregar extensões de forma assíncrona

--- main.py
+++ main.py
@@ -3,24 +3,21 @@ load_dotenv()
 from src.DiscordBot import DiscordBot
 import asyncio
 
-async def setup():
-    bot_instance = DiscordBot()
-    bot = bot_instance.init_bot()
-    
-    # Adicionar referência do bot_instance ao bot
-    bot.bot_instance = bot_instance
-
+async def setup_extensions(bot):
     # Carregue as extensões de forma assíncrona
     await bot.load_extension("src.cogs.music")
     await bot.load_extension("src.cogs.chat")
-    # ...carregue mais Cogs aqui...
 
-    return bot, bot_instance
-
-if __name__ == "__main__":
-    # Configurar e iniciar o bot de forma assíncrona
-    loop = asyncio.get_event_loop()
-    bot, bot_instance = loop.run_until_complete(setup())
+def main():
+    bot_instance = DiscordBot()
+    bot = bot_instance.init_bot()
+    
+    bot.bot_instance = bot_instance
+    
+    asyncio.run(setup_extensions(bot))
     
     print("Running bot...")
     bot.run(bot_instance.TOKEN)
+
+if __name__ == "__main__":
+    main()
